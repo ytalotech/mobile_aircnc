b896f507c15ed2895950890eb7af34b2baa50c8b
lopessoftec <lopessoftec@gmail.com>
2019-11-08 23:06:58 -0300

    listando os locais

--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { SafeAreaView, ScrollView, StyleSheet, Image, AsyncStorage } from 'react-native';
+import socketio from 'socket.io-client';
+import { Alert, SafeAreaView, ScrollView, StyleSheet, Image, AsyncStorage } from 'react-native';
 
 import SpotList from '../components/SpotList';
 
@@ -7,6 +8,20 @@ import logo from '../assets/logo.png';
 
 export default function List(){
     const [techs, setTechs] = useState([]);
+
+    useEffect(() => {
+        //quando eu tiver o user_id
+        AsyncStorage.getItem('user').then(user_id => {
+            const socket = socketio('http://192.168.0.9:3333', {
+                query: { user_id }
+            })
+
+            //toda vez que eu receber uma mensagem com booking_response irei exibir um alerta
+            socket.on('booking_response', booking => {
+                Alert.alert(`Sua reserva em ${booking.spot.company} em ${booking.date} foi ${booking.approved ? 'APROVADA' : 'REJEITADA' }`)
+            })
+        })
+    }, []);
 
     useEffect(() => {
         AsyncStorage.getItem('techs').then(storagedTechs => {
